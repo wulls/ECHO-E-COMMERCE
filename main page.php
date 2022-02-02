11c5576204007f48ebabd928efbc2f2fb2394136
wulan <cheezekukiz@gmail.com>
2022-02-02 19:35:44 +0700

    done

---
+++ main page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title> Trolley Main Page </title>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel = "stylesheet" href = "css/main page.css">
+</head>
+
+<body>
+ <div class="greenbg">
+  <img src="" class="logo">
+   <button onclick="document.location='#'">Daftar</button>
+  <button onclick="document.location='#'">Masuk</button>
+ </div>
+</body>
