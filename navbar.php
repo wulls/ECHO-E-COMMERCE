d4b93180badf71218f8441f910e4704ed4d87b68
wulan <cheezekukiz@gmail.com>
2022-02-16 17:49:58 +0700

    update

--- navbar.php
+++ navbar.php
@@ -6,13 +6,9 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Landing Page - Start Bootstrap Theme</title>
-        <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
-        <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/main page.css" rel="stylesheet" />
     </head>
 
--- navbarlogin.php
+++ navbarlogin.php
@@ -6,13 +6,9 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Landing Page - Start Bootstrap Theme</title>
-        <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
-        <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/main page.css" rel="stylesheet" />
     </head>
 
@@ -20,6 +16,6 @@
             <div class="container">
                 <a class="navbar-brand" href="#!"></a>
                 <img src="image/logo.png" class="logo">
-                <a class="btn btn-primary" href="#">Profile</a>
+                <a class="btn btn-primary" href="login.php">Profile</a>
             </div>
         </nav>
