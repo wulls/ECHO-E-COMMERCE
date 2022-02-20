caff347a5afff0eae7eabed2dc1b748bd835f91b
ernestisthen <ernestisthen@gmail.com>
2022-02-20 08:00:26 +0700

    up

--- index.php
+++ index.php
@@ -21,7 +21,17 @@ require_once ('indexcomponent.php');
   <link rel="stylesheet" href="CSS/index.css">
 </head>
 <body>
-<h1> Good morning </h1>
+  <div class="container">
+    <div class="row text-center py-5">
+      <?php
+       $sql = "SELECT * FROM product";
+       $result = mysqli_query($con,$sql);
+       while($row=mysqli_fetch_array($result)){
+      component($row['productName'], $row['productPrice'], $row['image']);
+    }
+       ?>
+    </div>
+  </div>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
