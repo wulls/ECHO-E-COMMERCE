2d55edabcd7a987ea63875ceaabee83b9de3b0dc
Lee-28 <albertsteven745@gmail.com>
2022-03-12 22:29:28 +0700

    Update kategori_update.php

--- admin/kategori_update.php
+++ admin/kategori_update.php
@@ -3,5 +3,5 @@ include '../database connection.php';
 $id  = $_POST['id'];
 $nama  = $_POST['name'];
 
-mysqli_query($koneksi, "update category set name='$nama' where category_id='$id'");
+mysqli_query($con, "update category set name='$nama' where category_id='$id'");
 header("location:kategori.php");
