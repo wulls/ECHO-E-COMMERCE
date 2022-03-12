b18d82f0685de2f03ea1000d2ed59392e1a59dac
Lee-28 <albertsteven745@gmail.com>
2022-03-12 22:29:15 +0700

    Update gantipassword_act.php

--- admin/gantipassword_act.php
+++ admin/gantipassword_act.php
@@ -1,9 +1,9 @@
 <?php 
-include '../koneksi.php';
+include '../database connection.php';
 session_start();
 $id = $_SESSION['id'];
 $password = md5($_POST['password']);
 
-mysqli_query($koneksi, "UPDATE admin SET admin_password='$password' WHERE admin_id='$id'")or die(mysqli_errno());
+mysqli_query($con, "UPDATE admin SET admin_password='$password' WHERE admin_id='$id'")or die(mysqli_errno());
 
 header("location:gantipassword.php?alert=sukses");
