d17bdfc2652112c133af21fc53cd35a3ffd7abd2
Lee-28 <albertsteven745@gmail.com>
2022-03-12 20:15:12 +0700

    Update produk_hapus.php

--- admin/produk_hapus.php
+++ admin/produk_hapus.php
@@ -1,28 +1,24 @@
 <?php 
 include '../database connection.php';
 $id = $_GET['id'];
-$data = mysqli_query($koneksi, "select * from produk where produk_id='$id'");
+$data = mysqli_query($con, "SELECT * from product where product_id='$id'");
 $d = mysqli_fetch_assoc($data);
-$foto1 = $d['produk_foto1'];
-$foto2 = $d['produk_foto2'];
-$foto3 = $d['produk_foto3'];
+$foto1 = $d['image'];
 
-unlink("../gambar/produk/$foto1");
-unlink("../gambar/produk/$foto2");
-unlink("../gambar/produk/$foto3");
 
-mysqli_query($koneksi, "delete from produk where produk_id='$id'");
+
+mysqli_query($con, "DELETE from product where product_id='$id'");
 
 
 
 
-$data = mysqli_query($koneksi, "select * from transaksi where transaksi_produk='$id'");
+$data = mysqli_query($con, "select * from transaksi where transaksi_produk='$id'");
 while($d=mysqli_fetch_array($data)){
 	$id_invoice = $d['transaksi_invoice'];
 
-	mysqli_query($koneksi, "delete from invoice where invoice_id='$id'");
+	mysqli_query($con, "delete from invoice where invoice_id='$id'");
 }
 
-mysqli_query($koneksi, "delete from transaksi where transaksi_produk='$id'");
+mysqli_query($con, "delete from transaksi where transaksi_produk='$id'");
 
 header("location:produk.php");
