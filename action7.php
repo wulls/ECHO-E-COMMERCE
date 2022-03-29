a9d53afd095774decb0003a375cfd4accdb0b12c
ernestisthen <ernestisthen@gmail.com>
2022-03-29 14:52:17 +0700

    hehe

---
+++ action7.php
@@ -0,0 +1,36 @@
+<?php include "database connection.php";
+//session_start();
+
+// $user_id = $_SESSION['user_id'];
+$pid = $_POST['pid'];
+$pname = $_POST['pname'];
+$pprice = $_POST['pprice'];
+$pimage = $_POST['pimage'];
+$pamount = $_POST['pamount'];
+$punit = $_POST['punit'];
+$pquantity = $_POST['pquantity'];
+
+$totalPrice = $pprice * $pquantity;
+
+$insertCart = "INSERT INTO cart (product_id, productName, productPrice, productImage, productAmount, productUnit, productQuantity, totalPrice)
+               VALUES ('$pid', '$pname', '$pprice', '$pimage', '$pamount', '$punit', '$pquantity', '$totalPrice')";
+$query = mysqli_query($con, $insertCart);
+
+/*echo $pid;
+echo "<br>";
+echo $pname;
+echo "<br>";
+echo $pprice;
+echo "<br>";
+echo $pimage;
+echo "<br>";
+echo $pamount;
+echo "<br>";
+echo $punit;
+echo "<br>";
+echo $pquantity;
+echo "<br>";
+echo $totalPrice;*/
+header("location:index7.php");
+
+?>
