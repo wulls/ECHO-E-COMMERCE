d5e81c7fc6d8ee3de9f94fea060bd336a6dda553
wulan <cheezekukiz@gmail.com>
2022-03-31 10:08:15 +0700

    user logout

--- newnavbarlogin.php
+++ newnavbarlogin.php
@@ -58,7 +58,7 @@
                 <hr class="dropdown-divider">
               </li>
               <li>
-                <a class="dropdown-item" href="#">Logout</a>
+                <a class="dropdown-item" href="user_logout.php">Logout</a>
               </li>
             </ul>
           </li>
