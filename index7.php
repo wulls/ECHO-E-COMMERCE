ea2c0684a816e51127de756b84b5c976c7d2b9ba
wulan <cheezekukiz@gmail.com>
2022-04-08 18:14:08 +0700

    beli dari & antar ke

--- index7.php
+++ index7.php
@@ -62,6 +62,7 @@ if (isset($_SESSION['user_id'])) {
   </div>
   <div class="container">
     <div class="row text-center py-5">
+
       <?php
       $merchant_id = $_SESSION['merchant_id'];
       //echo $merchant_id;
@@ -132,5 +133,139 @@ if (isset($_SESSION['user_id'])) {
       ?>
     </div>
   </div>
+  <div class="modal fade bd-example-modal-lg" id="merchantlist" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content" style="border-radius: 1rem;">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalScrollableTitle">Pilih Supermarket</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="container-fluid">
+            <div class="row">
+              <?php 
+                $sql = "SELECT * FROM merchant;";
+                $result = mysqli_query($con,$sql);
+                while($row=mysqli_fetch_array($result)){
+              ?>
+              <div class="col-md-4 d-flex justify-content-center" style="padding-top:10px;">
+                <form action="main page2.php" method="get">
+                  <div class="card" style="width:10rem;height:10rem;border-radius:.9rem;">
+                    <input type="image" src=<?php echo $row['image'];?> class="card-img-top" style="width:10rem;height:10rem;">
+                    <div class="card-body">
+                      <h5 class="text-center" style="font-size:15px;padding-bottom:40px;"><?php echo $row['merchantName']; ?></h5>
+                      <input type="hidden" name="merchant_id" value=<?php echo $row['merchant_id']; ?> >
+                    </div>
+                  </div><br><br><br>
+                </form>
+              </div>
+              <?php } ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal fade bd-example-modal-lg" id="addresslist" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content" style="border-radius: 1rem;">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalScrollableTitle">Pilih Alamat Pengantaran</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="container-fluid">
+            <div id="noAddress" style="display:none;">
+              <div class="d-flex justify-content-center">
+                <img src="image/document.png" width="200px;">
+              </div>
+              <div class="d-flex justify-content-center">
+                <p style="padding-top:15px;">Belum ada alamat yang tersimpan</p>
+              </div>
+              <div class="d-flex justify-content-center">
+                <form action="test_userprofile.php">
+                  <input class="but-ton" type="submit" value="Tambah Alamat">
+                </form>
+              </div>
+            </div>
+              <?php
+                $sql = "SELECT * FROM customeraddress WHERE customer_id='$user_id';";
+                $result = mysqli_query($con,$sql);
+                $count = mysqli_num_rows($result);
+
+                if($count == 0){
+                  echo "<script type=\"text/javascript\">
+                          document.getElementById('noAddress').style.display='block';
+                        </script>
+               ";
+                }
+
+                if($count > 0){
+                  while($row=mysqli_fetch_array($result)){
+              ?>
+              <div class="card" style="width:100%;">
+                <div class="card-body" style="padding-top:30px;">
+                  <div class="row">
+                    <div class="col-sm-9" style="width:40rem;">
+                      <b><?php echo $row['addressName']; ?></b>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-9" style="width:40rem;">
+                      <?php echo $row['recipientName']; ?>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-9" style="width:40rem;">
+                      <?php echo $row['addressDetail']; ?>
+                    </div>
+                  </div>
+                  <div class="ml-auto p-2 d-flex justify-content-end">
+                    <form action="" method="get">
+                      <input type="submit" value="Pilih Alamat">
+                      <input type="hidden" name="addressID" value=<?php echo $row['address_id']; ?>>
+                    </form>
+                  </div>
+                </div>
+              </div><br>
+              <?php } } ?>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <style type="text/css">
+    .logotoko{
+	    border-radius: 10px;
+    }
+    .card {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      min-width: 0;
+      word-wrap: break-word;
+      background-color: #fff;
+      background-clip: border-box;
+      border: 0 solid transparent;
+      border-radius: .9rem;
+    }
+    .card {
+    box-shadow: 0 2px 6px 0 rgb(218 218 253 / 65%), 0 2px 6px 0 rgb(206 206 238 / 54%);
+    }
+    .but-ton{
+    background-color: #2F86A6;
+    border-radius: 3px;
+    border:0;
+    color:white;
+    min-width: 8rem;
+    max-width: 15rem;
+    height: 2.5rem;
+    }
+  </style>
 </body>
 </html>
