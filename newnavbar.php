f14f5c463e43924240080ef3c07cd88020731c33
ernestisthen <ernestisthen@gmail.com>
2022-04-14 11:07:49 +0700

    newnavbar: index.php and styling

--- newnavbar.php
+++ newnavbar.php
@@ -6,12 +6,24 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  .cart{
+    color:white;
+  }
+  #navbarDropdown{
+    color:white;
+  }
+  #cart-item{
+    background-color: white;
+    color: #2F86A6;
+  }
+  </style>
 </head>
 <body>
 
   <nav class="navbar navbar-expand-lg" style="background-color:#34BE82">
     <div class="container">
-      <a class="navbar-brand" href="main page.php">
+      <a class="navbar-brand" href="index.php">
         <img src="image/logo2.svg" alt="Trolley" height="70">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +33,7 @@
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">
-              Cart
+              <i class="fas fa-shopping-cart cart"></i>
             </a>
           </li>
           <li class="nav-item dropdown">
