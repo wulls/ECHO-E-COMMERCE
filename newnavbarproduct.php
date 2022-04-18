750dbd8fa2d09abbd2574c71b053e3a470e26061
ernestisthen <ernestisthen@gmail.com>
2022-04-18 16:14:20 +0700

    comment on action, store, & newnavbarproduct

--- newnavbarproduct.php
+++ newnavbarproduct.php
@@ -146,5 +146,25 @@
   }
   </script>
 
+  <!--<script type="text/javascript">
+  $(document).ready(function() {
+    // Load total no.of items added in the cart and display in the navbar
+    load_cart_item_number();
+
+    function load_cart_item_number() {
+      $.ajax({
+        url: 'action.php',
+        method: 'get',
+        data: {
+          cartItem: "cart_item"
+        },
+        success: function(response) {
+          $("#cart-item").html(response);
+        }
+      });
+    }
+  });
+</script>-->
+
 </body>
 </html>
